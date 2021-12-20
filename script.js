ee8c4b9d8d30b63571b4fbb37322d32760496019
Wojciech Kowański <kowanski.wojciech@gmail.com>
2021-12-20 20:17:36 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+//https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b8225b041c82080be7743e54e195af9a
+const cityName = document.querySelector("h1");
+const deg = document.querySelector(".degrees");
+const clar = document.querySelector(".clarity");
+const submit = document.querySelector(".load");
+const cityInput = document.querySelector(".city");
+
+//weather
+async function getWeather(city) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=b8225b041c82080be7743e54e195af9a&units=metric`
+  );
+  const data = await response.json();
+  if (city === "") {
+    return;
+  }
+  cityName.textContent = `${data.name}`;
+  deg.textContent = ` ${Math.floor(data.main.temp)}\xB0C`;
+  clar.textContent = `${data.weather[0].description}`;
+  if (data.main.temp > 20) {
+    document.body.style.background =
+      "linear-gradient(to right, #ee0979, #ff6a00)";
+  } else {
+    document.body.style.background =
+      "linear-gradient(to right, #2F80ED, #56CCF2)";
+  }
+}
+
+submit.addEventListener("click", () => {
+  getWeather(cityInput.value);
+  cityInput.value = "";
+});
+
+//toDo
+
+const taskInput = document.querySelector(".doer");
+const addBtn = document.querySelector(".add");
+const ul = document.querySelector("ul");
+const toDoList = [];
+
+function taskHandler(e) {
+  e.preventDefault();
+
+  if (taskInput.value === "") return;
+  const task = document.createElement("li");
+  task.innerHTML = taskInput.value + " <button class='done'>DONE</button>";
+  toDoList.push(task);
+  ul.appendChild(task);
+  taskInput.value = "";
+  task.querySelector(".done").addEventListener("click", taskDone);
+}
+
+function taskDone(e) {
+  const element = e.target;
+  toDoList.splice(element, 1);
+}
+
+addBtn.addEventListener("click", taskHandler);
